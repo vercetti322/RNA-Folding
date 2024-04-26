0d1fba86057efdb87d7777a19b66d566bd75cb2f
vercetti322 <vercettitommy322@gmail.com>
2024-04-26 20:32:05 +0530

    Visualization tool started!

--- SDL2/window_renderer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <SDL2/SDL.h>
-
-SDL_Window* initWindow(const char* title, int width, int height);
-SDL_Renderer* initRenderer(SDL_Window* window);
-void handleEvents(bool& quit);
